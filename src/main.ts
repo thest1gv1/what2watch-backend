eb98fed413490faf218209b171d6aa27c91d2f45
thest1gv1 <thest1gv1@gmail.com>
2026-04-10 20:20:25 +0500

    backend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import {AppModule} from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://what2watch-rose.vercel.app'
+    ],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
