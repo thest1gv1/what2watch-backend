44f0fa5eb7bac2cfe290971494fbb68fc53eb270
thest1gv1 <thest1gv1@gmail.com>
2026-04-11 12:06:55 +0500

    backend

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,10 +1,12 @@
-import {Controller, Post, Body, Get} from '@nestjs/common';
-import { MoviesService } from './movies.service';
+import {Controller, Post, Body, Get, Res, Query} from '@nestjs/common';
+import type { Response } from 'express'
+import {MoviesService} from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
-  constructor(private readonly moviesService: MoviesService) {}
+  constructor(private readonly moviesService: MoviesService) {
+  }
 
   @Post('recommend')
   async recommend(@Body() body: { prompt: string }) {
@@ -19,5 +21,13 @@ export class MoviesController {
   @Get('popular')
   async popular() {
     return this.moviesService.getPopular()
+  }
+
+  @Get('poster')
+  async poster(@Query('path') path: string, @Res() res: Response) {
+    const imageResponse = await fetch(`https://image.tmdb.org/t/p/w300${path}`)
+    const buffer = await imageResponse.arrayBuffer()
+    res.set('Content-Type', 'image/webp')
+    res.send(Buffer.from(buffer))
   }
 }
